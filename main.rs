b889382d7df12376836431fcaf07de4f8bc73477
Diogo Sousa <diogogsousa@gmail.com>
2014-10-06 03:02:29 +0100

    A few more things getting serialized on the version message.

--- main.rs
+++ main.rs
@@ -1,3 +1,5 @@
+extern crate time;
+
 use std::io::TcpStream;
 
 mod marshalling;
@@ -8,6 +10,12 @@ mod config
     pub static VERSION: &'static str = "0.0";
 
     pub static PROTOCOL_VERSION: u32 = 70002;
+
+    enum Services {
+        NodeNetwork = 1 << 0,
+    }
+
+    pub static SERVICES: Services = NodeNetwork;
 }
 
 mod message
@@ -16,58 +24,38 @@ mod message
 
     pub struct Version
     {
-        version: &'static str
-    }
-
-    fn serialize_uint32(v: u32) -> [u8, ..4]
-    {
-        let l0: u8=(v&0xff) as u8;
-        let l1: u8=((v>>8)&0xff) as u8;
-        let l2: u8=((v>>8*2)&0xff) as u8;
-        let l3: u8=((v>>8*3)&0xff) as u8;
-
-        [l0,l1,l2,l3]
-    }
-
-    fn serialize_uint64(v: u64) -> [u8, ..8]
-    {
-        let l0: u8=(v&0xff) as u8;
-        let l1: u8=((v>>8)&0xff) as u8;
-        let l2: u8=((v>>8*2)&0xff) as u8;
-        let l3: u8=((v>>8*3)&0xff) as u8;
-        let l4: u8=((v>>8*4)&0xff) as u8;
-        let l5: u8=((v>>8*5)&0xff) as u8;
-        let l6: u8=((v>>8*6)&0xff) as u8;
-        let l7: u8=((v>>8*7)&0xff) as u8;
-
-        [l0,l1,l2,l3,l4,l5,l6,l7]
+        version: &'static str,
+        time:    ::time::Tm
     }
 
     impl Version
     {
         pub fn new(version: &'static str) -> Version
         {
-            Version { version: version }
+            Version { version: version,
+                      time: ::time::now_utc() }
         }
 
-        pub fn serialize(&self) // -> &[u8]
+        pub fn serialize(&self) -> Vec<u8>
         {
-            let VERSION : [u8, ..12] = [0x76,0x65,0x72,0x73,0x69,0x6F,0x6E,
-                                        0x00,0x00,0x00,0x00,0x00];
-/*/
-            let mut header : Vec<u8> = Vec::new();
-            let mut msg : Vec<u8> = Vec::new();
-
-            // TODO make message / make header functions
-            header.push_all(MAIN_NET);
-            header.push_all(VERSION);
-
-            header+msg
-*/
             let mut header = ::marshalling::Marshalling::new();
+            let mut msg = ::marshalling::Marshalling::new();
 
             header.write(MAIN_NET);
-            header.write(VERSION);
+            header.write_str12("version");
+            // payload size
+            // checksum
+
+            msg.write_uint32(::config::PROTOCOL_VERSION);
+            msg.write_uint64(::config::SERVICES as u64);
+            msg.write_int64(self.time.to_timespec().sec);
+            // recipient addr
+            // sender addr
+            // node id
+            msg.write_varstr(self.version);
+            // last block
+
+            header.get() + msg.get()
         }
     }
 }
@@ -76,7 +64,7 @@ fn send_version(socket : &mut TcpStream)
 {
     let version = message::Version::new(config::NAME);
 
-    // socket.write(version.serialize().as_slice());
+    socket.write(version.serialize().as_slice());
 }
 
 fn main()
